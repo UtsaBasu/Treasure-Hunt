17ce82587bc591db0e7c5b80c67224980a420fb1
UtsaBasu <76468540+UtsaBasu@users.noreply.github.com>
2021-05-24 16:27:51 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,19 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+    
+text("A T D A S B A", 100,300)
+text("Hint: Where you can store different files and data !!", 100,330)
+
+text("C U T N I F O N", 700,50)
+text("Hint: Performs a particular task !!", 700,70)
+}
